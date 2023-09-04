42294e6840f1799cb1b89138a02f0b2a305c31fa
Oscar Vasquez <oscardavid.vp@gmail.com>
2023-09-04 12:41:48 +0200

    resistor color duo

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,6 @@
+COLOR_LEGEND = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
 def value(colors):
-    pass
+    # list comprehension of the first two colors in the list,
+    # convert each element to str then join them into a string, then convert to int
+    return int(''.join([ str(COLOR_LEGEND.index(code)) for code in colors[:2] ]))
