9643ed6a06dcd4ab1ee9d69a28e387f4e179bb10
Oscar Vasquez <oscardavid.vp@gmail.com>
2023-09-21 13:31:18 +0200

    encode vlq

--- variable-length-quantity/variable_length_quantity.py
+++ variable-length-quantity/variable_length_quantity.py
@@ -1,6 +1,28 @@
-def encode(numbers):
-    pass
+def encode(numbers: list) -> list:
 
+    result = []
 
-def decode(bytes_):
+    for number in numbers:
+        as_int = int(number)
+        if (as_int < 128):
+            result.append(number)
+        
+        else:
+            vlq = bin(as_int)[2:] ## remove starting 0b..
+            aux, aux_encode = vlq, []
+
+            while aux:
+                byte = aux[-7:]
+
+                byte = '1' + '0'*(7-len(byte)) + byte \
+                    if len(aux) != len(vlq) else byte
+                
+                aux_encode.append(int(byte,2)) # specific base
+                aux = aux[:-7] # reduce aux size
+            
+            result.extend(aux_encode[::-1]) # extend with the inverted list
+
+    return result
+
+def decode(bytes_: list) -> list:
     pass
