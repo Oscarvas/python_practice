09fb3c84ae0b1f2b5b76db112f3171dac03f50cf
Oscar Vasquez <oscardavid.vp@gmail.com>
2023-09-21 15:39:43 +0200

    decode vlq

--- variable-length-quantity/variable_length_quantity.py
+++ variable-length-quantity/variable_length_quantity.py
@@ -12,10 +12,10 @@ def encode(numbers: list) -> list:
             aux, aux_encode = vlq, []
 
             while aux:
-                byte = aux[-7:]
+                byte = aux[-7:] # get last 7 bits
 
                 byte = '1' + '0'*(7-len(byte)) + byte \
-                    if len(aux) != len(vlq) else byte
+                    if len(aux) != len(vlq) else byte # only the first 7-bits (from right) is 0xxxxxxx
                 
                 aux_encode.append(int(byte,2)) # specific base
                 aux = aux[:-7] # reduce aux size
@@ -25,4 +25,22 @@ def encode(numbers: list) -> list:
     return result
 
 def decode(bytes_: list) -> list:
-    pass
+    result = []
+    
+    bits = ''
+
+    for number in bytes_:
+        clean_number = bin(number)[2:][-7:]  # extract 0b first, then get last 7 bits
+
+        if number > 127:
+            bits += clean_number
+        
+        else:
+            bits += '0'*(7-len(clean_number)) + clean_number
+            result.append(int(bits,2))
+            bits = ''
+
+    if bits:
+        raise ValueError("incomplete sequence")
+
+    return result
